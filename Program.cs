5ab312ecaa03293b276b9b10eae33ae3a2b07c0c
soldiereleven <soldiereleven@outlook.com>
2025-04-01 22:12:13 +0800

    init: ASP.NET empty template

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace OpenBMCLAPI_IN
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
